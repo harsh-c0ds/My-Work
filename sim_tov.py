f8cf71bafcff2c46406133e23e95b51eaebe2b68
Harsh Solanki <139675852+pibond008@users.noreply.github.com>
2025-12-12 16:38:11 +0000

    power

--- sim_tov.py
+++ sim_tov.py
@@ -149,10 +149,10 @@ output_dir = "/home/hsolanki/Programs/My-Work/output/"
 t,x_p,rl,rl_n,datax = get_info("hydrobase","vel",sim_dir,0.0,"x")
 time_values_vel,vel_values = fx_timeseries(t,x_p,datax,10,"x")
 
-time_values_vel = np.array(time_values_vel)/(203*1000)  # convert to s
+time_values_vel = np.array(time_values_vel)/203  # convert to ms
 vel_values = np.array(vel_values)  # in units of c
 
-frequency_vel = np.linspace(0.01, 9000, 5000)  # 0–16 kHz
+frequency_vel = np.linspace(0.01, 9, 5000)  # 0–9 kHz
 vel = LombScargle(time_values_vel, vel_values).power(frequency_vel)
 
 plt.figure(figsize=(8,6))
