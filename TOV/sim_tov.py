2c707caa306108a1f95fc63a8d128da09dafb787
pibond008 <harshvsolanki8111@gmail.com>
2026-01-08 12:31:58 +0100

    changes

--- TOV/sim_tov.py
+++ TOV/sim_tov.py
@@ -202,14 +202,14 @@ t_s = np.array(time_values)/203
 print(len(t_s), len(rho))
 
 plt.figure(figsize=(8,6))
-plt.plot(t_s, rho, color="red", linewidth=1.5, label="Ideal Fluid EOS")
+plt.plot(t_s, rho, color="red", linewidth=1.5, label="Polytropic EOS")
 #plt.plot(t, rho, color="blue", linewidth=1.5, label="data read")
 plt.xlabel("Time (ms)")
 plt.ylabel(r"$\rho/\rho_{c,0}$")
 plt.title(r"Timeseries of Density")
 plt.grid(True, linestyle=":")
 plt.legend()
-plt.savefig(output_dir + "time_series_density_IF.png", dpi=300)
+plt.savefig(output_dir + "time_series_density_P.png", dpi=300)
 
 
 ### Power Spectrum ###
@@ -221,13 +221,13 @@ rho_p = (np.array(f_xt_values) - f_xt_values[0]) / f_xt_values[0]
 t_s = np.array(time_values)/203
 
 freq_p, power_p = fourier_transform(t_s, rho_p)
-power_smooth = gaussian_filter1d(power_p, sigma=3)
+power_smooth = gaussian_filter1d(power_p, sigma=2)
 # peaks, _ = find_peaks(power_p, height=np.max(power_p)*0.1, distance=20)
 # labels = ["F", "H1", "H2", "H3", "H4", "H5"]
 peaks, properties = find_peaks(
     power_smooth,
-    prominence=np.max(power_smooth) * 0.05,  # stands out from background
-    width=5                                   # suppress narrow noise spikes
+    prominence=np.max(power_smooth) * 0.015,  # stands out from background
+    width=2                                   # suppress narrow noise spikes
 )
 labels = ["F", "H1", "H2", "H3", "H4", "H5"]
 
