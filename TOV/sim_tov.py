2a25e184ee8f27942c57f20546467190d15d86cf
Harsh Solanki <139675852+pibond008@users.noreply.github.com>
2026-01-19 13:36:56 +0000

    changes

--- TOV/sim_tov.py
+++ TOV/sim_tov.py
@@ -185,7 +185,7 @@ def fourier_transform(time_ms, rho, n_freq=3000):
 
 sim_dir_if = "/home/hsolanki/simulations/tov_IF/output-0000/tov_ET"
 sim_dir_p = "/home/hsolanki/simulations/Pol_sim/output-0000/tov_ET"
-sim_dir_lean = "/home/hsolanki/simulations/lean_bssn/output-0000/tov_ET"
+sim_dir_lean = "/home/hsolanki/simulations/lean_bssn/output-0001/tov_ET"
 output_dir = "/home/hsolanki/Programs/My-Work/output/"
 
 # itr, t, rho = np.loadtxt('/home/hsolanki/simulations/Pol_sim/output-0000/tov_ET/hydrobase-rho.maximum.asc', unpack=True, comments='#')
