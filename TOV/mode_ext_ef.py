f638b10f82d1d0033801ca4a12c768b646792f23
Harsh Solanki <139675852+pibond008@users.noreply.github.com>
2026-01-26 14:09:06 +0000

    changes

--- TOV/mode_ext_ef.py
+++ TOV/mode_ext_ef.py
@@ -219,7 +219,7 @@ peaks_l, properties = find_peaks(
 peaks_adm, properties = find_peaks(
     power_adm,
     height=np.max(power_adm) * 0.06,
-    prominence=np.percentile(power_adm, 95) * 0.2,
+    prominence=np.percentile(power_adm, 95) * 0.18,
     width=7.0,
 )
 # peaks_adm, properties = find_peaks(
