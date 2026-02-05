3bd574c10aa570fdb9b303406b4576b4fac3f0da
PiBonD008 <harshvsolanki8111@gmail.com>
2026-02-05 17:04:24 +0100

    Updated - 2026-02-05 17:04:23

--- TOV/sim_tov.py
+++ TOV/sim_tov.py
@@ -265,7 +265,7 @@ time_values,f_xt_values = fx_timeseries(t,x_p,datax,0,"x")
 output_file = output_dir + "vel_timeseries_Lean.txt"
 
 with open(output_file, "w") as f:
-    for i in range(0):
+    for i in range(78,78):
         time_values, f_xt_values = fx_timeseries(t, x_p, datax, i, "x")
 
         f_xt_values = np.array(f_xt_values)
